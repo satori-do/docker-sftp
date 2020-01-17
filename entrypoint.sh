720c6eadeb923fe420af8a2be47beb1d291c629a
Mykola Bilokonenko <enet.tier@gmail.com>
2020-01-17 14:47:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 printf "\n\033[0;44m---> Creating SFTP user.\033[0m\n"
 
 addgroup sftp
-mkdir /uploads/${SFTP_USER}
-mkdir /uploads/${SFTP_USER}/sftp
+mkdir -p /uploads/${SFTP_USER}
+mkdir -p /uploads/${SFTP_USER}/sftp
 useradd -d /uploads/${SFTP_USER} -G sftp ${SFTP_USER} -s /usr/sbin/nologin
 echo "${SFTP_USER}:${SFTP_PASS}" | chpasswd
 chown ${SFTP_USER}:sftp -R /uploads/${SFTP_USER}/sftp
