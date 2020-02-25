c3c3556d9d17c4bc59c6fc828e3b334b8958a00e
satori-do <volodymyrreznich@gmail.com>
2020-02-25 17:56:10 +0200

    Ownership or modes for chroot directory component was corrected.

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,14 @@ for i in "${!users[@]}"; do
   echo Creating user ${users[i]}
   mkdir -p /uploads/${users[i]}/sftp
   useradd -d /uploads/${users[i]} -G sftp ${users[i]} -s /usr/sbin/nologin
+  chown root:${users[i]} /uploads/${users[i]}
+  chmod 750 /uploads/${users[i]}
   chown ${users[i]}:sftp -R /uploads/${users[i]}/sftp
   echo "${users[i]}:${passwords[i]}" | chpasswd
   echo User ${users[i]} created
 done
 
 printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
-
-# service ssh start
-# service ssh status
 
 mkdir -p /run/sshd
 
